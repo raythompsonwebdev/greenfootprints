0316d9b18aeb2784fd912aea7b846905b50b52d5
DigbyCeaser <4f7baea1@opayq.com>
2016-09-20 01:52:28 +0100

    new Changes

---
+++ feedback-green-foot-prints-com.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="minimum-scale=0">
+<meta name="viewport" content="user-scalable=yes">
+<meta name="viewport" content="target-densitydpi=device-dpi">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="Raymond Thompson" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="copyright" content="&copy;raymondthompson" />
+
+<title>Greenfoot-Prints-Feedback-Page</title>
+
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/text.css" />
+<link rel="stylesheet" href="css/960_24_col.css" />
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="css/animate.css" />
+<link href="Fonts/fontawesome/css/font-awesome.min.css" rel="stylesheet">
+
+<!--HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+ <!--WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+<!--[if IE]>
+ <link rel="stylesheet" type="text/css" href="ie.css"/>
+ <script src="js/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+ <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.js"></script>
+<![endif]-->
+
+</head>
+<body>
+
+<section class="container_24">
+
+<header>
+
+<section class="socials">
+
+<ul>
+  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+    <li><a href="#"><i class="fa fa-google"></i></a></li>
+  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+</ul>
+
+</section>
+<figure id="logo"><img src="img/logo-green-foot-prints.gif" alt="logo-green-foot-prints"></figure>
+<hgroup>
+<h1> GreenFootPrints</h1>
+<h2>Recycling website</h2>
+</hgroup>
+
+</header>
+
+ 
+<nav role="navigation">
+  <ul>
+    <li><a href="index.html" >Home</a></li>
+      <li><a href="recycling-green-foot-prints-com.html" >Recycling</a></li>
+        <li><a href="collections-green-foot-prints-com.html" >Collections</a></li>
+        <li><a href="ten-ways-to-recycle-green-foot-prints-com.html" >10 Ways</a></li>
+        <li><a href="recycling-facts-green-foot-prints-com.html" >Recyling Facts</a></li>
+      <li><a href="feedback-green-foot-prints-com.php" >Feedback</a></li>
+    <li><a href="contact-us-green-foot-prints-com.html" >Contact</a></li>
+  </ul>
+</nav>
+
+
+  <!-- end .grid_24 -->
+  <div class="clear"> </div>
+ 
+  
+  <!-- end .grid_6 -->
+<div class="grid_24">
+  
+ 
+    
+<section id="content" >
+    	
+<article class="text">
+
+  <h1>Feedback</h1>
+
+   
+<form action="" method="get"  >
+  <ul>
+    
+    <li><label for="">Name</label>
+    <input type="text"></li>
+
+    <li><label for="">Email</label>
+    <input type="text"></li>
+    
+    <li><label for="">Subject</label>
+    <input type="text"></li>
+        
+    <li><label for="">Message</label>
+    <textarea name="textarea" rows="10"></textarea></li>
+
+    <li><label for=""></label><br/>
+    <input type="submit" name="Submit" id="Submit" value="Submit" />
+   </li>
+   <li></li>
+
+  </ul>
+</form>
+
+
+
+
+
+<br/>
+<br/>
+</article>
+    	
+
+<div class="clear"></div>
+
+</section>
+    
+<div class="clear"></div>
+    
+</div><!-- end .grid_18 -->
+
+<div class="clear"></div>
+  
+  
+<section class="grid_24">
+  <footer>
+   <ul>
+      <li><a href="index.html" >Home</a></li>
+        <li><a href="recycling-green-foot-prints-com.html" >Recycling</a></li>
+          <li><a href="collections-green-foot-prints-com.html" >Collections</a></li>
+          <li><a href="ten-ways-to-recycle-green-foot-prints-com.html" >10 Ways</a></li>
+          <li><a href="recycling-facts-green-foot-prints-com.html" >Recyling Facts</a></li>
+        <li><a href="feedback-green-foot-prints-com.php" >Feedback</a></li>
+      <li><a href="contact-us-green-foot-prints-com.html" >Contact</a></li>
+    </ul>
+  </footer>
+</section>
+  <p class="copyr"> 
+  &copy; 2015 - www.raythompwebdesign.com
+<br/>
+<script type="text/javascript">document.write("Page Last Updated: " + document.lastModified);</script>
+</p>
+</section>
+<!-- end .container_24 -->
+
+<!--- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+<script type='text/javascript'>window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js">\x3C/script>')
+</script>
+
+</body>
+</html>
