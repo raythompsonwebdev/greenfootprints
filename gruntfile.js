8a88c6966981ad71c6f995f963dd5f144078b8c1
Raymond Thompson <ray_thomp@hushmail.com>
2017-12-09 23:37:55 +0000

    Changes

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,92 @@ module.exports = function(grunt){
 
             pkg: grunt.file.readJSON('package.json'),
 		
+		
+		responsive_images: {
+		  dev: {
+			options: {
+			  engine: 'im',
+			  sizes: [
+				  
+				{
+				  name:'quantamsmall',
+				  width: 80,
+				},
+				{
+				  name:'microsmall',
+				  width: 120,
+				},
+				{
+				  name: 'extrasmall',
+				  width: 240,
+				},
+				{
+				  name:'small',
+				  width: 500,
+				},
+				{
+				  name:'medium',	
+				  width: 980,
+				},
+			  	{
+				  name:'large',	
+				  width: 1200,
+				}]
+			},
+
+			/*
+			You don't need to change this part if you don't change
+			the directory structure.
+			*/
+			files: [{
+			  expand: true,
+			  src: ['*.{gif,jpg,png}'],
+			  cwd: 'greenfootprints-app/img/',
+			  dest: 'greenfootprints-app/images/'
+			}]
+		  }
+		},
+
+		/* Clear out the images directory if it exists */
+		clean: {
+		  dev: {
+			src: ['images'],
+		  },
+		},
+
+		/* Generate the images directory if it is missing */
+		mkdir: {
+		  dev: {
+			options: {
+			  create: ['images']
+			},
+		  },
+		},
+		
+		
+		htmlmin: {                                     // Task
+			dist: {                                      // Target
+			  options: {                                 // Target options
+				removeComments: true,
+				collapseWhitespace: true
+			  },
+			  files: {                                   // Dictionary of files
+				'greenfootprints-app/dist/index.html': 'greenfootprints-app/index.html',     // 'destination': 'source'
+				'greenfootprints-app/dist/recycling-green-foot-prints-com.html': 'greenfootprints-app/recycling-green-foot-prints-com.html',
+				'greenfootprints-app/dist/recycling-facts-green-foot-prints-com.html': 'greenfootprints-app/recycling-facts-green-foot-prints-com.html',   
+				'greenfootprints-app/dist/recycling-ettiquette-green-foot-prints-com.html': 'greenfootprints-app/recycling-ettiquette-green-foot-prints-com.html',   
+				'greenfootprints-app/dist/feedback-green-foot-prints-com.html': 'greenfootprints-app/feedback-green-foot-prints-com.html',  
+				'greenfootprints-app/dist/contact-us-green-foot-prints-com.html': 'greenfootprints-app/contact-us-green-foot-prints-com.html',  
+				'greenfootprints-app/dist/collections-green-foot-prints-com.html': 'greenfootprints-app/collections-green-foot-prints-com.html',   
+				'greenfootprints-app/dist/500.html': 'greenfootprints-app/500.html',   
+				'greenfootprints-app/dist/404.html': 'greenfootprints-app/404.html',  
+				'greenfootprints-app/dist/401.html': 'greenfootprints-app/401.html',   
+				'greenfootprints-app/dist/ten-ways-to-recycle-green-foot-prints-com.html': 'greenfootprints-app/ten-ways-to-recycle-green-foot-prints-com.html',     
+				  
+			  }
+			}
+		  },
+
 		/**
          * Concat
          */
@@ -13,8 +99,8 @@ module.exports = function(grunt){
                 separator: ';'
             },
             dist: {
-                src: ['js/**/custom.js'],
-                dest: 'dist/<%= pkg.name %>.js'
+                src: ['greenfootprints-app/js/**/*.js'],
+                dest: 'greenfootprints-app/dist/<%= pkg.name %>.js'
             }
         },
         
@@ -25,17 +111,20 @@ module.exports = function(grunt){
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-                }
-            }
+			
+			my_target: {
+			  files: {
+				'greenfootprints-app/dist/output.min.js': ['greenfootprints-app/js/custom.js', 'greenfootprints-app/js/jquery.easing.1.3.js', 'greenfootprints-app/js/jquery.flexslider.js']
+			  }
+			},
+			
+            
         },
 
-            /**
-             * sass Task
-             */
-            sass:{
+		/**
+		 * sass Task
+		 */
+        sass:{
 
                 dev:{
                     options:{
@@ -62,7 +151,7 @@ module.exports = function(grunt){
                     }
                 },
 		
-              /**
+         /**
          * QUnit
          */
 
@@ -87,32 +176,40 @@ module.exports = function(grunt){
             }
         },
 
-            watch:{
+        /**
+         * watch
+         */
+		watch:{
 
                 css:{
                         files:'greenfootprints-app/**/*.scss',
-                        tasks:['sass']
+                        tasks:['sass', 'jshint', 'htmlmin', 'uglify', 'concat', 'responsive_images' ]
                 }
             }
 
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-	
-	grunt.registerTask('test', ['jshint', 'qunit']);
-   
-   grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-   grunt.registerTask('default', ['sass', 'watch']);
+grunt.loadNpmTasks('grunt-responsive-images');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-mkdir');
+grunt.loadNpmTasks('grunt-contrib-htmlmin');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-qunit');
+grunt.loadNpmTasks('grunt-contrib-concat');
+
+grunt.registerTask('test', ['jshint', 'qunit']);
+
+grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'clean', 'mkdir', 'htmlmin']);
+grunt.registerTask('default', ['sass', 'watch' ,'responsive_images']);
 	
 	
 
 
-}
+};
 
 /* add bag (!) to wordpress css theme top-title so that it shows on minified file*/
