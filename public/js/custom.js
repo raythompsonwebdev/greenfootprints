37dd4cc23a659e4f8d5dd41302effe53feda1cbc
raythompsonwebdev <ray_thomp@hushmail.com>
2019-04-07 01:28:03 +0100

    update

---
+++ public/js/custom.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+
+    $('button#mobile-toggle').bind('click', function (event) {
+
+        event.preventDefault();
+
+        $('ul#mobile-nav').slideToggle(250).css('display', 'block');
+
+    });
+
+    /* ========================================== 
+    scrollTop() >= 100
+    Should be equal the the height of the header
+    ========================================== */
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 90) {
+
+            $('nav').addClass('fixed-header');
+            $('div.grid_24').css('padding-top', '65px');
+            $('nav div').addClass('nav-title');
+        }
+        else {
+            $('nav').removeClass('fixed-header');
+            $('div.grid_24').css('padding-top', '0');
+            $('nav div').removeClass('nav-title');
+        }
+    });
+
+    // Add smooth scrolling to all links
+    $(".nav-links").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+
+            });
+        } // End if
+    });
+
+    $("#myBtn").on('click', function (event) {
+
+        $('html,body').animate({scrollTop:0},800);
+
+    });
+
+});
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    //function topFunction() {
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    //}
