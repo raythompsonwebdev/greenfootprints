932334271c8718bf8e88c0cebfdb00d710e1c2c2
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2024-01-21 02:09:42 +0000

    updated missing rate limiting

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -24,10 +24,10 @@ const limiter = rateLimit({
 app.use(limiter);
 
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname + '/dist')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 //opens browser
